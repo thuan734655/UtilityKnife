388331db76138c75c0a34836564ba43c051f382e
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-02-29 09:00:23 +0700

    add some function

--- Caculator/caculator.cs
+++ Caculator/caculator.cs
@@ -1,10 +1,26 @@
 namespace UtilityKnife.Calculator
 {
- public class Calculator
+ public static class Calculator
  {
- public static int Add (int x, int y)
+ public static double Add(double x, double y)
  {
  return x + y;
+ }
+ public static double Subtract(double x, double y)
+ {
+ return x - y;
+ }
+ public static double Multiply(double x, double y)
+ {
+ return x * y;
+ }
+ public static int Division (int x, int y)
+ {
+ return x / y;
+ }
+ public static double Division (double x, double y)
+ {
+ return x / y;
  }
  }
 }
